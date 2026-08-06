cc56ba3c657159907d0d10742bec04f430715e42
HenrikGharagyozyan1 <henrik.gharagyozyan1@gmail.com>
2026-08-06 17:01:40 +0400

    core: add import for ChatGoogleGenerativeAI in main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langgraph.prebuilt import create_react_agent
+from langchain_google_genai import ChatGoogleGenerativeAI
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
